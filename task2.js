f69b9a01bbe65e48c149ef99a5e8b9ef64b25b5b
Oksana Semko <osemko@gmail.com>
2022-05-06 01:44:57 +0300

    task2_solution

---
+++ task2.js
@@ -0,0 +1,31 @@
+// Доповни функцію makeMessage таким чином, щоб вона очікувала
+// другим параметром(параметр callback) колбек - функцію і повертала її виклик.
+// Функція deliverPizza або makePizza буде передаватися як колбек
+// і очікувати аргументом ім'я готової піци, що доставляється.
+
+-----------------------------------------------------------------------------------
+
+// Оголошена функція deliverPizza
+// Оголошена функція makePizza
+// Оголошена функція makeMessage
+// Функція makeMessage приймає два параметри, названі відповідно до завдання,
+// pizzaName і callback
+// Виклик makeMessage("Royal Grand", makePizza) повертає рядок
+// "Pizza Royal Grand is being prepared, please wait..."
+// Виклик makeMessage("Ultracheese", deliverPizza) повертає рядок
+// "Delivering Ultracheese pizza."
+
+---------------------------------------------------------------------------------------
+
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+}
